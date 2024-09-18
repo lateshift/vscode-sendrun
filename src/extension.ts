16f3ca5d5bf0709c74c7a825e50382edfa84f9c2
Jens C <179465+lateshift@users.noreply.github.com>
2024-09-18 15:41:34 +0200

    Better description :)

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
         const terminal = vscode.window.activeTerminal;
         const fileName = activeEditor.document.fileName;
         terminal?.sendText(`run ${fileName}`);
-        // vscode.window.showInformationMessage(`Current file: ${fileName}`);
       } else {
         vscode.window.showInformationMessage("No file - no execute 🫥");
       }
